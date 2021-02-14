069b072c9966d497a48c364dce19a9fa61aef2c6
Marco Jansen <marco.jansen@gmx.us>
2021-02-14 12:38:38 +0800

    added functions to modules

--- server.js
+++ server.js
@@ -2,17 +2,6 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const baseSelection = require("./lib/baseSelection");
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-  
-    port: 3306,
-  
-    user: 'root',
-  
-    password: '@kina1987',
-    database: 'employee_db',
-  });
-
-  baseSelection();
+baseSelection();
 
 
