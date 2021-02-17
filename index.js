6a1d2a7f50687d0f8cc509de0f33678749d138d4
Marco Jansen <marco.jansen@gmx.us>
2021-02-17 15:09:42 +0800

    all employeeOperations bugfixed

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
-const baseSelection = require("./lib/baseSelection");
+const selectTask = require("./lib/inquirer-helpers");
+const {addEmployee, viewEmployees, deleteEmployee, updateEmployee} = require("./lib/employeeOperations");
+const {addRole, viewRoles, deleteRole, updateRole} = require("./lib/roleOperations");
+const {addDepartment, viewDepartments, deleteDepartment, updateDepartment} = require("./lib/departmentOperations");
 
-baseSelection();
+const employeeOperations = {
+    'Show all employees': viewEmployees,
+    'Add new employee': addEmployee,
+    'Update existing employee': updateEmployee,
+    'Delete existing employee': deleteEmployee,
+    'Back to task selection': baseSelection,  
+  }
+  
+  const roleOperations = {
+    'Show all roles': viewRoles,
+    'Add new role': addRole,
+    'Update existing role': updateRole,
+    'Delete existing role': deleteRole,
+    'Back to task selection': baseSelection,
+  };
+  
+  const departmentOperations = {
+    'Show all departments': viewDepartments,
+    'Add new department': addDepartment,
+    'Update existing department': updateDepartment,
+    'Delete existing department': deleteDepartment,
+    'Back to task selection': baseSelection,
+  };
 
+  function baseSelection() {
+    selectTask({
+      "Manage Employee information": () => selectTask(employeeOperations), 
+      "Manage roles": () =>  selectTask(roleOperations), 
+      "Manage departments": () =>  selectTask(departmentOperations),
+    })
+   
+  };
+
+  baseSelection();
